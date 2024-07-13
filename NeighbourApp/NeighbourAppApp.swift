bca95a168d7c215245bbd0f1c2dc31f1deb1234f
IanTranPersonal <vinhxtran1@gmail.com>
2024-07-14 00:21:51 +1000

    Initial Commit

---
+++ NeighbourApp/NeighbourAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  NeighbourAppApp.swift
+//  NeighbourApp
+//
+//  Created by Vinh Tran on 14/7/2024.
+//
+
+import SwiftUI
+
+@main
+struct NeighbourAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
